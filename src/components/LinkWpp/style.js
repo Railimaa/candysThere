7a281e16f5ce8f208bc2029db731ec362769e1ec
iaramil <iaramil@h3r3ys-MacBook-Pro.local>
2023-07-12 19:38:37 -0300

    add api

--- src/components/LinkWpp/style.js
+++ src/components/LinkWpp/style.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   a {
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: flex-start;
     gap: 4px;
 
       color: ${({ theme }) => theme.colors.terc};
