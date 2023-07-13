557cc59f63dd182ec2bbe77a8e86b94bea981e6b
iaramil <iaramil@h3r3ys-MacBook-Pro.local>
2023-07-13 17:03:58 -0300

    changes dispositivos responsivos

--- src/components/WhatsappIcon/style.js
+++ src/components/WhatsappIcon/style.js
@@ -1,34 +1,23 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.colors.terc};
-  color: white;
-  font-size: 24px;
-  z-index: 9999;
-  text-decoration: none;
-
-
-
-
-
-  @media (min-width: 768px) {
+  a {
     position: fixed;
     bottom: 20px;
     right: 20px;
-  }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.colors.terc};
+    color: white;
+    font-size: 24px;
+    z-index: 9999;
+    text-decoration: none;
 
-  @media (min-width: 992px) {
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
+
+
   }
 `;
